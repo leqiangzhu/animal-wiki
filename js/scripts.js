09064c5a2cdfd313bacf6b94fef26a23732a1bd0
Akjol Jaenbai and david zhu <david@>
2018-08-09 10:35:01 -0700

    stuck, will work on it later

---
+++ js/scripts.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+  var animal = parseInt($("#beverage").val());
+  alert("11111111")
+
+  if (animal === 1) {
+    $('#turtles').show();
+  else if('.animals').hide();
+
+  // } else if (animal === 2) {
+  //   $('#snakes').show();
+  //   $(".animal").hide();
+  //
+  // }else if (animal ==== 3) {
+  //   $('#insects').show();
+  //   $(".animal").hide();
+  //
+  // }
+    event.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+
+//   if($("#turtles").click(function(){
+//     $("#turtle".show();}
+//   else{}
+//
+// });
+//     $("#").hide();
+//     $("#snakes").toggle();
+//     //$("#snakes").hide();
+//
+//
+//   }else if($(".Snakes").click(function()){
+//         $("#").hide();
+//           $("#snakes").toggle();
+//
+//   }
+
+
+  // $(".turtle").click(function(){
+  //   // $("#snakes").hide();
+  //   // $("#insects").hide();
+  //   $("#turtles").toggle();
+  //     var checkturtle=ture;
+  //
+  // });
+
+
+  //
+  // $(".Snakes").click(function(){
+  //   // $("#turtles").hide();
+  //   // $("#insects").hide();
+  //   $("#snakes").toggle();
+  //     var checktsnakes=ture;
+  // });
+  // $(".Insects").click(function(){
+  //   // $("#snakes").hide();
+  //   // $("#turtles").hide();
+  //   $("#insects").toggle();
+  // });
+  //
+  //   if (checkturtle=ture){
+  //     $("#").hide();
+  //   }else if(checktsnakes=ture){
+  //     $("#Insects").show();
+  //   }
+  //
+
+
+
+
+//
+// });
